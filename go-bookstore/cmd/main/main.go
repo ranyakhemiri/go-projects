e8d2f94cca86c337bfb13d94f7931ce79b6834af
ranyakhemiri <rania.khemiri@datadoghq.com>
2024-04-18 14:09:00 +0200

    Bookstore managed by mysql API working

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/ranyakhemiri/go-bookstore/pkg/routes"
 )
 
@@ -13,7 +14,7 @@ import (
 func main() {
 	// creating a new router
 	r := mux.NewRouter()
-	routes.RegisterBookStoreRoutes(r)
+	routes.RegisterBookstoreRoutes(r)
 	http.Handle("/", r)
 	// listen on port 9010, if there is an error, log it
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
