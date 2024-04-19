0fb4db07da2d2f03df3d721668f9e5cdccddc464
ranyakhemiri <rania.khemiri@datadoghq.com>
2024-04-19 13:05:16 +0200

    age bot on slack final commit'

--- slack-age-bot/main.go
+++ slack-age-bot/main.go
@@ -6,8 +6,9 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"github.com/shomali11/slacker"
+
 	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
 )
 
 func printCommandEvents(analytisChannel <-chan *slacker.CommandEvent) {
@@ -20,30 +21,39 @@ func printCommandEvents(analytisChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", godotenv.Load(".env")["SLACK_BOT_TOKEN"])
-	os.Setenv("SLACK_APP_TOKEN", godotenv.Load(".env")["SLACK_APP_TOKEN"])
+	// Load environment variables
+	errr := godotenv.Load(".env")
+	if errr != nil {
+		log.Fatalf("Error loading .env file: %v", errr)
+	}
+
+	slackBotToken := os.Getenv("SLACK_BOT_TOKEN")
+	slackAppToken := os.Getenv("SLACK_APP_TOKEN")
+	os.Setenv("SLACK_BOT_TOKEN", slackBotToken)
+	os.Setenv("SLACK_APP_TOKEN", slackAppToken)
+
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("My year of birth is <year>", &slacker.CommandDefinition){
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
 		Description: "Set your year of birth",
-		Example:     "My year of birth is 1990",
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			year := request.Param("year")
 			yob, err := strconv.Atoi(year)
 			if err != nil {
 				println("Error: ", err)
-				
+
 			}
 			age := 2024 - yob
 			r := fmt.Sprintf("Your age is %d", age)
 			response.Reply(r)
-		}
-	}
+		},
+	})
 
 	// stop bot
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	err := bot.Listen(ctx)
 	if err != nil {
 		log.Fatal(err)
